65c5684a6785d669a06f47180fcdb5c9300013e5
Matt Hibshman <matthibshman@gmail.com>
2021-06-07 15:11:39 -0400

    initial Firefighter implementation

--- Firefighters_v1_(3)/src/main/firefighters/FirefighterImpl.java
+++ Firefighters_v1_(3)/src/main/firefighters/FirefighterImpl.java
@@ -2,19 +2,38 @@ package main.firefighters;
 
 import main.api.CityNode;
 import main.api.Firefighter;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 public class FirefighterImpl implements Firefighter {
 
+  private CityNode location;
+  private int distance;
+
+  public FirefighterImpl(CityNode location) {
+    this.location = location;
+    this.distance = 0;
+  }
+
   @Override
   public CityNode getLocation() {
-    // TODO
-    throw new NotImplementedException();
+    return location;
   }
 
   @Override
   public int distanceTraveled() {
-    // TODO
-    throw new NotImplementedException();
+    return distance;
+  }
+
+  // HANDLE INVALID LOCATION???
+  public void dispatch(CityNode location) {
+    calculateDistance(this.location, location);
+    this.location = location;
+  }
+
+  private void calculateDistance(CityNode fromLocation, CityNode toLocation) {
+    distance += getDistance(fromLocation, toLocation);
+  }
+
+  private int getDistance(CityNode fromLocation, CityNode toLocation) {
+    return Math.abs(fromLocation.getX() - toLocation.getX()) + Math.abs(fromLocation.getY() - toLocation.getY());
   }
 }
